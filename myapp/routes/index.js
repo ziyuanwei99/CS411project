74e463aee3d9126bc6b33cd82734048cb2c3c9fc
ziyuanw2 <ziyuanw2@github.com>
2020-05-05 02:16:17 -0500

    h

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -2,23 +2,6 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql')
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'developer',
-  password: 'cs411',
-  database: 'concert_db'
-})
-
-connection.connect()
-
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-  if (err) throw err
-
-  console.log('The solution is: ', rows[0].solution)
-})
-
-connection.end()
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
