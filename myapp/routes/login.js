c228ee194b6ca1d39249a4c848723b2e09a92464
ziyuanw2 <ziyuanw2@github.com>
2020-05-05 00:56:56 -0500

    h

---
+++ myapp/routes/login.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/', function(req, res) {
+  var username=req.body.username
+  var password=req.body.password
+  //mysql query
+  res.send(JSON.stringify({status: "good"}))
+});
+
+module.exports = router;
